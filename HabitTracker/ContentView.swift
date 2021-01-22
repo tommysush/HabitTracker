3483dd93dac1b8a9c56215b9169f4bf395d93f19
Tommy SuSH <tommy.sush@gmail.com>
2021-01-22 22:00:35 +0800

    test commit

---
+++ HabitTracker/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  HabitTracker
+//
+//  Created by 蘇聖泓 on 2021/1/22.
+//
+
+import SwiftUI
+
+let message = "Hello2, Git"
+
+struct ContentView: View {
+    var body: some View {
+        Text(message)
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
